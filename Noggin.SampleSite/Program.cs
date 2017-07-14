2988f541e43a3aeed8062f71f49f1e3478bd1a07
Richard Garside <richard.garside@nogginbox.co.uk>
2017-07-14 08:25:38 +0100

    Removed Application Insights

--- Noggin.SampleSite/Program.cs
+++ Noggin.SampleSite/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO;
 using Microsoft.AspNetCore.Hosting;
 
 namespace Noggin.SampleSite
@@ -16,7 +12,6 @@ namespace Noggin.SampleSite
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
-                .UseApplicationInsights()
                 .Build();
 
             host.Run();
